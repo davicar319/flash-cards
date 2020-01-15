9a64f83ac701cff7c055f82d709f2e2356af36e9
Dave Carlson <dave.carlson@tr.com>
2020-01-15 14:22:12 -0600

    Code cleanup and Logging.

--- src/main/java/com/bixbysdoghouse/flashcards/repository/FlashCardRepositoryImpl.java
+++ src/main/java/com/bixbysdoghouse/flashcards/repository/FlashCardRepositoryImpl.java
@@ -4,26 +4,39 @@ import com.bixbysdoghouse.flashcards.entity.FlashCard;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Slf4j
 public class FlashCardRepositoryImpl implements FlashCardRepository{
+
     private final Set<FlashCard> flashCards = new HashSet<>();
     private long currentId = 0;
 
     @Override
     public FlashCard save(FlashCard flashCard) throws CardExistsException {
+        log.debug(">>>save: flashCard={}", flashCard);
         if(flashCards.contains(flashCard)) {
             throw new CardExistsException("The FlashCard: " + flashCard + " already exists in the system.");
         } else {
             flashCard.setId(++currentId);
             flashCards.add(flashCard);
         }
+        log.debug("<<<save: flashcard={} -> {}", flashCard, flashCard);
         return flashCard;
     }
 
     @Override
     public FlashCard findById(Long id) throws FlashCardNotFoundException {
-        return flashCards.stream().filter(flashCard -> flashCard.getId().equals(id)).findFirst().orElseThrow(FlashCardNotFoundException::new);
+        log.debug(">>>findById: id={}", id);
+        FlashCard result =  flashCards.stream()
+                                      .filter(flashCard -> flashCard.getId().equals(id))
+                                      .findFirst()
+                                      .orElseThrow(FlashCardNotFoundException::new);
+        log.debug(">>>findById: id={} -> {}", id, result);
+        return result;
     }
 }
